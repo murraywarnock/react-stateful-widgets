1c94f1f39bb3f2da99598646fc97e2db9fef7946
MurrayWarnock <murray.warnock@gmail.com>
2021-04-12 21:43:58 -0600

    completed counter.js, spinner.js, input.js. Started input.js, squares.js

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -37,11 +37,11 @@ STEP 4:
   Do you remember the operator we use to do "not"?
 */
 
-import React from 'react'; /* STEP 0 */
+import React, {useState} from 'react'; /* STEP 0 */
 
 export default function Spinner() {
 /* STEP 1 */
-
+const [spinnerOn, setSpinnerOn] = useState(true);
   const toggleSpinner = () => {
   /* STEP 4 */
   };
@@ -50,10 +50,11 @@ export default function Spinner() {
     <div className='widget-spinner container'>
       <h2>Spinner</h2>
       {
-        true && <div id='spinner' className='spinner'>--+--</div> /* STEP 2 */
+        // true && <div id='spinner' className='spinner'>--+--</div> /* STEP 2 */
+        spinnerOn && <div id='spinner' className='spinner'>--+--</div> /* STEP 2 */
       }
-      <button id='toggleSpinner' onClick={toggleSpinner}>
-        Hide Spinner {/* STEP 3 */}
+      <button id='toggleSpinner' onClick={() => setSpinnerOn(!spinnerOn)}>
+        {spinnerOn ? "Hide" : "Show"} Spinner {/* STEP 3 */}
       </button>
     </div>
   );
